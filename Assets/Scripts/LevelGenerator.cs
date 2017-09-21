016cd9420c58da7558f67f23924caa1484763138
Grant Wang <yidegrant@gmail.com>
2017-09-21 19:21:10 -0400

    Preliminary Map Generation Complete.
    May require tweaking. Levels created are generally very sparse. May attempt a room based version.

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -4,54 +4,98 @@ using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour {
 
-    public int mapHalfWidth;
-    public int mapHalfHeight;
+    public LevelMaterials myLevelData;
+    public Vector2[] playerStartPos;
 
-    public Vector2 mapCenter;
-
-    public Transform[] blockTypes;
     List<Vector2> critPath = new List<Vector2>();
 
-    public Vector2[] playerStartPos;
+    [Range(1, 4)]
     public int tempPlayerCount;
     
-    [Range(0.1f, 0.9f)]
-    public float blockDensity;
-
 	// Use this for initialization
 	void Start () {
         // Generate player locations
         playerStartPos = new Vector2[] {
-            new Vector2(-mapHalfWidth, mapHalfHeight),
-            new Vector2(mapHalfWidth, mapHalfHeight),
-            new Vector2(-mapHalfWidth, -mapHalfHeight),
-            new Vector2(mapHalfWidth, -mapHalfHeight),
+            new Vector2(-myLevelData.mapHalfWidth, myLevelData.mapHalfHeight),
+            new Vector2(myLevelData.mapHalfWidth, myLevelData.mapHalfHeight),
+            new Vector2(-myLevelData.mapHalfWidth, -myLevelData.mapHalfHeight),
+            new Vector2(myLevelData.mapHalfWidth, -myLevelData.mapHalfHeight),
         };
-        for(int i = 0; i < tempPlayerCount; i++)
+        shuffle(playerStartPos);
+        for(int i = 0; i < playerStartPos.Length; i++)
         {
+            Debug.Log("Generating Path for Player " + i + "...");
             generateCritPath(playerStartPos[i]); // Create a critical path from player's spawn to center
         }
-        for(int i = -mapHalfHeight; i < mapHalfHeight; i++)
+        for(int i = 0; i < playerStartPos.Length; i++)
         {
-            for(int j = -mapHalfWidth; j < mapHalfWidth; j++)
-            {
-                Vector2 newLoc = new Vector2(j, i);
-                if (!critPath.Contains(newLoc))
-                {
-
-                }
-            }
+            Transform newPlayHighlight = Instantiate(myLevelData.playerHighlight, playerStartPos[i], Quaternion.identity);
+            newPlayHighlight.position -= Vector3.forward;
         }
+        buildLevel();
+        StartCoroutine(visualizeCritPath());
 	}
+
+    IEnumerator visualizeCritPath()
+    {
+        for (int i = 0; i < critPath.Count; i++)
+        {
+            Transform newCritHighlight = Instantiate(myLevelData.critPathHighlight, critPath[i], Quaternion.identity);
+            newCritHighlight.position -= Vector3.forward;
+            yield return new WaitForSeconds(0.1f);
+        }
+        Debug.Log("FINISHED!");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-    void spawnNewTile()
+    void buildLevel()
     {
-        
+        for (int i = -myLevelData.mapHalfWidth; i < myLevelData.mapHalfWidth; i++)
+        {
+            for (int j = -myLevelData.mapHalfHeight; j < myLevelData.mapHalfHeight; j++)
+            {
+                Vector2 newLoc = new Vector2(i, j);
+                if (!critPath.Contains(newLoc))
+                {
+                    spawnNewTile(newLoc);
+                }
+            }
+        }
+        for(int i = -myLevelData.mapHalfWidth - 1; i <= myLevelData.mapHalfWidth; i++)
+        {
+            Transform borderTileT = Instantiate(myLevelData.solidTiles[0], new Vector2(i, myLevelData.mapHalfHeight + 1), Quaternion.identity);
+            Transform borderTileB = Instantiate(myLevelData.solidTiles[0], new Vector2(i, -myLevelData.mapHalfHeight - 1), Quaternion.identity);
+        }
+        for (int i = -myLevelData.mapHalfHeight - 1; i <= myLevelData.mapHalfHeight + 1; i++)
+        {
+            Transform borderTileL = Instantiate(myLevelData.solidTiles[0], new Vector2(-myLevelData.mapHalfWidth - 1, i), Quaternion.identity);
+            Transform borderTileB = Instantiate(myLevelData.solidTiles[0], new Vector2(myLevelData.mapHalfWidth + 1, i), Quaternion.identity);
+        }
+    }
+
+    void spawnNewTile(Vector2 loc)
+    {
+        float chance = myLevelData.blockDensity;
+        bool closeToPlayer = false;
+        Transform[] tilesList;
+        for(int i = 0; i < playerStartPos.Length; i++)
+        {
+            if(Vector2.Distance(loc, playerStartPos[i]) < 4f)
+            {
+                closeToPlayer = true;
+                break;
+            }
+        }
+        tilesList = conCat(myLevelData.solidTiles, myLevelData.weakTiles);
+        if (!closeToPlayer) { tilesList = conCat(tilesList, myLevelData.dangerTiles); }
+        if(Random.value < chance)
+        {
+            Transform newTile = Instantiate(tilesList[Random.Range(0, tilesList.Length - 1)], loc, Quaternion.identity);
+        }
     }
 
     class Vertex
@@ -75,14 +119,17 @@ public class LevelGenerator : MonoBehaviour {
             new Vector2(0, -1),
             new Vector2(-1, 0),
         };
-        while(currentVertex != null)
+        while(toBeSearched.Count > 0)
         {
             currentVertex = toBeSearched.Pop();
-            if(currentVertex.location == mapCenter) // If we've reached the center
+            if (currentVertex.location == myLevelData.mapCenter) // If we've reached the center
             {
                 while(currentVertex != null)
                 {
-                    critPath.Add(currentVertex.location); // Backtrack through the vertices to add to critical path
+                    if (!critPath.Contains(currentVertex.location))
+                    {
+                        critPath.Add(currentVertex.location); // Backtrack through the vertices to add to critical path
+                    }
                     currentVertex = currentVertex.parent;
                 }
                 return;
@@ -91,20 +138,21 @@ public class LevelGenerator : MonoBehaviour {
             for(int i = 0; i < dirs.Length; i++)
             {
                 Vector2 newLoc = currentVertex.location + dirs[i];
-                if (isInMap(newLoc) && !alreadySearched.Contains(newLoc)
-                    && !critPath.Contains(newLoc))
+                if (isInMap(newLoc) && !alreadySearched.Contains(newLoc))
                 {
                     Vertex newVertex = new Vertex();
                     newVertex.location = newLoc;
                     newVertex.parent = currentVertex;
+                    toBeSearched.Push(newVertex);
                 }
             }
+            alreadySearched.Add(currentVertex.location);
         }
     }
 
     bool isInMap(Vector2 loc)
     {
-        return (Mathf.Abs(loc.x) < mapHalfWidth && Mathf.Abs(loc.y) < mapHalfHeight);
+        return (Mathf.Abs(loc.x) <= myLevelData.mapHalfWidth && Mathf.Abs(loc.y) <= myLevelData.mapHalfHeight);
     }
 
     void shuffle<T>(T[] leArray)
@@ -116,5 +164,19 @@ public class LevelGenerator : MonoBehaviour {
             leArray[i] = leArray[rand];
             leArray[rand] = temp;
         }
+    }
+
+    T[] conCat<T>(T[] arr1, T[] arr2)
+    {
+        T[] newArr = new T[arr1.Length + arr2.Length];
+        for(int i = 0; i < arr1.Length; i++)
+        {
+            newArr[i] = arr1[i];
+        }
+        for(int i = 0; i < arr2.Length; i++)
+        {
+            newArr[i + arr1.Length] = arr2[i];
+        }
+        return newArr;
     }
 }
---
+++ Assets/Scripts/LevelMaterials.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelMaterials")]
+public class LevelMaterials : ScriptableObject {
+
+    public Transform[] solidTiles;
+    public Transform[] weakTiles;
+    public Transform[] dangerTiles;
+
+    public Transform playerHighlight;
+    public Transform critPathHighlight;
+
+    public Vector2[] dirs;
+
+    [Range(0.1f, 0.9f)]
+    public float blockDensity;
+    
+    [Range(8, 30)]
+    public int mapHalfWidth;
+    [Range(8, 30)]
+    public int mapHalfHeight;
+    
+
+    public Vector2 mapCenter;
+}
