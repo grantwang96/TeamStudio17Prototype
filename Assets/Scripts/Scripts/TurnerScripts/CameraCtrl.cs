a67b427b77497323ef5550a80e19aecb5ab296c0
Unknown <tgr235@nyu.edu>
2017-09-28 16:17:44 -0400

    Auto stash before merge of "DankBranch" and "origin/master"

---
+++ Assets/Scripts/Scripts/TurnerScripts/CameraCtrl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour {
+
+	public List<Transform> otherPlayers;
+//	private transform avgTransform;
+
+	// Use this for initialization
+	void Start () {
+//		for (i = 0; i < otherPlayers.Count; i++){
+//			
+//		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Assets/Scripts/Scripts/TurnerScripts/MovementSript2.cs
+++ Assets/Scripts/Scripts/TurnerScripts/MovementSript2.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementScript : MonoBehaviour {
+public class MovementSript2 : MonoBehaviour {
+
 
 	public float movementspeed = 1.0f;
-
-
+	public float jumpForce = 10f;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey(KeyCode.LeftArrow))
+		if (Input.GetKey(KeyCode.A))
 		{
 			transform.position += Vector3.left * movementspeed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.RightArrow))
+		if (Input.GetKey(KeyCode.D))
 		{
 			transform.position += Vector3.right * movementspeed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.UpArrow))
+		if (Input.GetKey(KeyCode.W))
 		{
 			transform.position += Vector3.up * movementspeed * Time.deltaTime;
 		}
-		if (Input.GetKey(KeyCode.DownArrow))
+		if (Input.GetKey(KeyCode.S))
 		{
 			transform.position += Vector3.down * movementspeed * Time.deltaTime;
 		}
+		if (Input.GetKey(Space)){
+			GetComponent<Rigidbody2D>().AddForce (new Vector2(0, jumpForce));
+		}
 	}
+
+
 }
