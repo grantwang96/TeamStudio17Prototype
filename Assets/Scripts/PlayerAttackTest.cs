cd266d0a73359f22dd6cea63e363c538ff370c91
Unknown <tgr235@nyu.edu>
2017-10-03 14:43:58 -0400

    Finishing the level proto

---
+++ Assets/Scripts/PlayerAttackTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackTest : MonoBehaviour {
+
+	public int attackPower = 6;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.collider.GetComponent<Block> () != null){
+			other.collider.GetComponent<Block> ().DestroyBlock (attackPower);
+		}
+	}
+}
