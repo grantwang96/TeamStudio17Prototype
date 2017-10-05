047afb3fefff3282eb2b723c581ef3d87af5d3b7
Gaberino <gabriel.m.rush@gmail.com>
2017-10-05 00:00:32 -0400

    Added Wings and Double Jump items

---
+++ Assets/Scripts/Scripts/Working Scripts/ChargeRollItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "StateReplacingItem/ChargeRollItem")]
+public class ChargeRollItem : StateReplacingItem {
+
+	public float chargeRate;
+	public float chargeToDurationRatio;
+	public float chargeToSpeedRatio;
+	private float rollDuration;
+	private float rollSpeed;
+
+	public override void RunFunction (Player caller)
+	{
+		if (!caller.rolling) {
+			caller.rollTimer = 0;
+			caller.rolling = true;
+			if (caller.facing > 0) {
+				caller.velocity = new Vector2 (rollSpeed, 0);
+			} else {
+				caller.velocity = new Vector2 (-rollSpeed, 0);
+			}
+		}
+		if (!caller.grounded) {
+			caller.rollTimer = 0;
+			caller.rolling = false;
+		}
+		else if (caller.rollTimer >= rollDuration) {
+			caller.rollTimer = 0;
+			caller.rolling = false;
+			caller.velocity = Vector2.zero;
+			caller.rollQueued = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Scripts/Working Scripts/WingItemScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ActionReplacingItem/WingsItem")]
+public class WingItemScript : ActionReplacingItem {
+
+	public float fromGroundJumpforce;
+	public float aerialFlapForce;
+
+	public override void RunFunction (Player caller) {
+
+		if (caller.grounded) {
+			caller.velocity += Vector2.up * fromGroundJumpforce;
+		}
+		else {
+			caller.velocity += Vector2.up * aerialFlapForce;
+		}
+	}
+}
