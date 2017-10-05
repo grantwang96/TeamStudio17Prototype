f10c6d36367a6197ab4c2d3b5c94ee9dbb7cf461
Grant Wang <yidegrant@gmail.com>
2017-10-05 13:08:16 -0400

    Spawning in maze gen level

--- Assets/Scripts/Scripts/TwoPlayerSpawn.cs
+++ Assets/Scripts/Scripts/TwoPlayerSpawn.cs
@@ -9,8 +9,13 @@ public class TwoPlayerSpawn : MonoBehaviour {
     public LevelGenerator levelGen;
     bool spawned = false;
 
-	// Use this for initialization
-	void Start () {
+    public CameraCtrl cameraControlScript;
+    public List<ActionReplacingItem> attacks = new List<ActionReplacingItem>();
+    public List<ActionReplacingItem> jumps = new List<ActionReplacingItem>();
+    public List<StateReplacingItem> rolls = new List<StateReplacingItem>();
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -26,6 +31,28 @@ public class TwoPlayerSpawn : MonoBehaviour {
     {
         Transform newPlayer1 = Instantiate(player1Prefab, levelGen.playerStartPos[0], Quaternion.identity);
         Transform newPlayer2 = Instantiate(player2Prefab, levelGen.playerStartPos[1], Quaternion.identity);
+        modifyLoadout(Random.Range(0, 3), newPlayer1.gameObject);
+        modifyLoadout(Random.Range(0, 3), newPlayer2.gameObject);
+        cameraControlScript.gameObject.SetActive(true);
+        cameraControlScript.p1 = newPlayer1.gameObject;
+        cameraControlScript.p2 = newPlayer2.gameObject;
+        cameraControlScript.p1cam = newPlayer1.Find("Camera").gameObject;
+        cameraControlScript.p2cam = newPlayer2.Find("Camera").gameObject;
         spawned = true;
+    }
+
+    void modifyLoadout(int val, GameObject player) {
+        if(val > 0)
+        {
+            player.GetComponent<Player>().JumpFunction = jumps[Random.Range(0, jumps.Count)];
+        }
+        if(val > 1)
+        {
+            player.GetComponent<Player>().AttackFunction = attacks[Random.Range(0, attacks.Count)];
+        }
+        if(val > 2)
+        {
+            player.GetComponent<Player>().RollFunction = rolls[Random.Range(0, rolls.Count)];
+        }
     }
 }
